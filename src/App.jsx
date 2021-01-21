b527204fe91368de8e1a96d45cddf782ad6b2145
Erik Sulimenko <erik.sulimenko@gmail.com>
2021-01-22 00:15:49 +0300

    add input, refactor reducer payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useNotify } from './Notifications';
 
@@ -6,13 +6,17 @@ import './App.css';
 
 function App() {
   const notify = useNotify();
-  
+
+  const [value, setValue] = useState('');
+
   const onClick = () => {
-    notify('test 123');
+    notify(value);
+    setValue('');
   };
 
   return (
     <div className="App">
+      <input type="text" onChange={({ target }) => setValue(target.value)} value={value} />
       <button
         type="button"
         onClick={onClick}
--- src/Notifications/hooks/useNotify.js
+++ src/Notifications/hooks/useNotify.js
@@ -10,7 +10,10 @@ export const useNotify = () => {
     (text) => {
       dispatch({
         type: ADD,
-        payload: text,
+        payload: {
+          id: Date.now(),
+          text,
+        },
       });
     },
     [dispatch],
